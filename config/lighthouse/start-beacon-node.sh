d763ff5f3f49c951a6fda010135a6e88e825ba1a
stefa2k <stefan.kobrc@rocklogic.at>
2020-11-05 12:44:26 +0100

    fix lighthouse metrics for 0.3.x

--- config/lighthouse/start-beacon-node.sh
+++ config/lighthouse/start-beacon-node.sh
@@ -5,31 +5,32 @@
 DEFAULT_TESTNET=medalla
 
 if [ "$START_VALIDATOR" != "" ]; then
-	ETH1_FLAG=--eth1
+        ETH1_FLAG=--eth1
 fi
 
 
 if [ "$TESTNET" = "" ]; then
-	TESTNET=$DEFAULT_TESTNET
+        TESTNET=$DEFAULT_TESTNET
 fi
 
-
-DATADIR=/root/.lighthouse/$TESTNET
+if [ "$ENABLE_METRICS" != "" ]; then
+        METRICS_PARAMS="--metrics --metrics-address 0.0.0.0"
+fi
 
 if [ "$GRAFFITI" != "" ]; then
-	GRAFFITI_PARAM="--graffiti $GRAFFITI"
+        GRAFFITI_PARAM="--graffiti $GRAFFITI"
 fi
 
 exec lighthouse \
-	--debug-level $DEBUG_LEVEL \
-	--testnet $TESTNET \
-	--datadir $DATADIR \
-	beacon_node \
-	--eth1-endpoint $VOTING_ETH1_NODE \
-	--http \
-	--http-address 0.0.0.0 \
-	--ws \
-	--ws-address 0.0.0.0 \
-	--max-skip-slots 700 \
-	$GRAFFITI_PARAM \
-	$ETH1_FLAG
+        --debug-level $DEBUG_LEVEL \
+        --testnet $TESTNET \
+        beacon_node \
+        --eth1-endpoint $VOTING_ETH1_NODE \
+        --http \
+        --http-address 0.0.0.0 \
+        $METRICS_PARAMS \
+        --ws \
+        --ws-address 0.0.0.0 \
+        --max-skip-slots 700 \
+        $GRAFFITI_PARAM \
+        $ETH1_FLAG
