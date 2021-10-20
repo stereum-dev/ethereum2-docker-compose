fce15435e61631cf5620a7694dc5e20e26e1410c
stefa2k <stefan.kobrc@rocklogic.at>
2021-10-20 15:53:12 +0200

    docker-compose -> docker for prysm

--- prysm_validator_delete.sh
+++ prysm_validator_delete.sh
@@ -2,6 +2,6 @@
 
 set validator_pubkey [lindex $argv 0];
 
-spawn sudo PUBKEY="$validator_pubkey" docker-compose -f delete-account.yaml run validator-delete-accounts
+spawn sudo docker exec -it stereum_validator_1 /app/cmd/validator/validator accounts delete --wallet-dir="/data/wallets" --wallet-password-file="/data/passwords/wallet-password" --delete-public-keys=validator_pubkey --accept-terms-of-use="true"
 
 expect eof
