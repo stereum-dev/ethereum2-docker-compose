e050d510e67d5cafbc4da900f574bc5a6bcc02f6
gbayasgalan <bayasgalan.gantumur@rocklogic.at>
2021-10-21 20:49:06 +0200

    fix teku voluntary exit

--- teku_validator_exit.sh
+++ teku_validator_exit.sh
@@ -4,7 +4,7 @@ set timeout 20
 
 set validator_key [lindex $argv 0];
 
-spawn sudo KEY="$validator_key" docker-compose -f exit-account.yaml run exit-validator
+spawn sudo docker exec -it stereum_beacon_1 /opt/teku/bin/teku voluntary-exit --beacon-node-api-endpoint=http://beacon:5051 --validator-keys=/launchpad/${validator_key}.json:/launchpad/${validator_key}.txt
 
 expect "Are you sure you wish to continue\r"
 
