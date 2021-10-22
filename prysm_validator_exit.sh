e17441affb2e4e054bcff4f949778e3518c66ff6
gbayasgalan <73820006+gbayasgalan@users.noreply.github.com>
2021-10-22 07:14:30 +0200

    fixing prysm account actions & teku voluntary exit (#300)
    * Altair ready client versions
    
    * fix missing teku max memory
    
    * updating lighthouse
    
    * updating geth
    
    * shellcheck setup
    
    * ignoring expect scripts for shellcheck
    
    * ignore erros of unsupported shell scripts
    
    * shell script cleanup
    
    * Lodestar reintegration (#298)
    
    * update lodestar
    
    * fixing config file
    
    * remove .json file
    
    * fixing beacon config
    
    * fixing validator config
    
    * add vol:launchpad to validator
    
    * fix remove validator
    
    * fix exit validator
    
    * fix import validator
    
    * update
    
    * fix teku voluntary exit

--- prysm_validator_exit.sh
+++ prysm_validator_exit.sh
@@ -2,7 +2,7 @@
 
 set validator_pubkey [lindex $argv 0];
 
-spawn sudo docker exec -it stereum_validator_1 /app/cmd/validator/validator accounts voluntary-exit --wallet-dir="/data/wallets" --wallet-password-file="/data/passwords/wallet-password" --public-keys=$validator_pubkey --accept-terms-of-use="true" --beacon-rpc-provider="beacon:4000"
+spawn sudo docker exec -it stereum_validator_1 /app/cmd/validator/validator accounts voluntary-exit --wallet-dir=/data/wallets --wallet-password-file=/data/passwords/wallet-password --public-keys=$validator_pubkey --accept-terms-of-use=true --beacon-rpc-provider=beacon:4000 --prater
 
 expect "If you still want to continue with the voluntary exit*\r"
 
